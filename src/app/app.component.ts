66b213d152cf6dafdae9488441e76f15df58ca1d
jelorivera08 <jerald@virtualcontrol.io>
2020-05-21 18:17:37 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DataStreamService } from "./data-stream.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'coco-sample';
+  title = "coco-sample";
+
+  constructor(private dataStreamService: DataStreamService) {}
+
+  getTweets() {
+    console.log(this.dataStreamService.getTweetsPerMinute());
+  }
+
+  ngOnInit() {
+    this.dataStreamService.initializeStream();
+  }
 }
---
+++ src/app/data-stream.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core";
+import PubNub from "pubnub";
+import { Tweet } from "./tweet";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataStreamService {
+  tweets: Tweet[] = [];
+  count = 0;
+
+  initializeStream() {
+    setInterval(() => this.count++, 1000);
+
+    var pubnub = new PubNub({
+      subscribe_key: "sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe",
+    });
+
+    pubnub.addListener({
+      message: (data) => {
+        this.tweets.push({
+          id: data.message.id,
+          content: data.message,
+          countryCode: data.message.place
+            ? data.message.place.country_code
+            : null,
+        });
+      },
+    });
+    pubnub.subscribe({
+      channels: ["pubnub-twitter"],
+    });
+  }
+
+  getTweetsPerMinute(): number {
+    return Math.round((this.tweets.length / this.count) * 60);
+  }
+
+  getTweets() {
+    return this.tweets;
+  }
+
+  ngOnInit() {}
+  constructor() {}
+}
